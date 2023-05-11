e3752aab14c409b9743b3a4d05fc3bf9ffdae297
Karim <kggm0116@gmail.com>
2023-05-11 19:01:18 +0300

    Refactored; Implemented Task 1.1.

---
+++ application/src/main/java/ru/kggm/launcher/LauncherActivity.kt
@@ -0,0 +1,30 @@
+package ru.kggm.launcher
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.kggm.helloworld.HelloWorldActivity
+import ru.kggm.launcher.databinding.ActivityLauncherBinding
+import ru.kggm.happybirthday.HappyBirthdayActivity
+
+class LauncherActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLauncherBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLauncherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.task11HomeworkButton.setOnClickListener {
+            startActivity(Intent(this, HelloWorldActivity::class.java))
+        }
+        binding.task11ChallengeButton.setOnClickListener {
+            startActivity(Intent(this, HappyBirthdayActivity::class.java))
+        }
+    }
+
+
+}
