84c391e971df357894fd113d30991c93a807652c
Karim <kggm0116@gmail.com>
2023-05-11 14:17:48 +0300

    Added bogus string resource id that gets filtered out and logged.

--- part_1_challenge/src/main/java/ru/kggm/part_1_challenge/MainActivity.kt
+++ part_1_challenge/src/main/java/ru/kggm/part_1_challenge/MainActivity.kt
@@ -1,29 +1,26 @@
 package ru.kggm.part_1_challenge;
 
+import android.content.Context
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log
 import androidx.annotation.StringRes
 import androidx.core.os.LocaleListCompat
 import ru.kggm.part_1_challenge.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val NAME_ID_BUNDLE_KEY = "nameId"
+        private const val DEFAULT_NAME_ID = R.string.name_karim
+    }
+
     private lateinit var binding: ActivityMainBinding
 
     @StringRes
-    private var nameId = R.string.name_karim
-
-    private fun getRandomNameId() = listOf(
-        R.string.name_arthur,
-        R.string.name_karim,
-        R.string.name_oleg
-    )
-        .random()
-
-    companion object {
-        private const val NAME_ID_BUNDLE_KEY = "nameId"
-    }
+    private var nameId = DEFAULT_NAME_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +38,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onLocalesChanged(locales: LocaleListCompat) {
         super.onLocalesChanged(locales)
-
         setBirthdayText()
     }
 
@@ -50,7 +46,38 @@ class MainActivity : AppCompatActivity() {
         outState.putInt(NAME_ID_BUNDLE_KEY, nameId)
     }
 
+    private fun getRandomNameId() = listOf(
+        R.string.name_arthur,
+        R.string.name_karim,
+        R.string.name_oleg,
+        "BOGUS".hashCode()
+    )
+        .partition { resourceExists(it) }
+        .let { (existing, notExisting) ->
+            notExisting.forEach { id ->
+                Log.e(
+                    "Getting random name id",
+                    "'$id' is not a valid id of a string resource"
+                )
+            }
+            existing.randomOrNull() ?: DEFAULT_NAME_ID
+        }
+
+    private fun Context.resourceExists(id: Int): Boolean {
+        try {
+            resources.getResourceName(id)
+        }
+        catch (e: Resources.NotFoundException) {
+            return false
+        }
+        return true
+    }
+
     private fun setBirthdayText() {
+        if (!resourceExists(nameId)) {
+            Log.e("Setting birthday text", "'$nameId' is not a valid id of a name resource")
+            return
+        }
         val localizedName = getString(nameId)
         binding.happyBirthdayText.text = getString(
             R.string.happy_birthday_text_template,
