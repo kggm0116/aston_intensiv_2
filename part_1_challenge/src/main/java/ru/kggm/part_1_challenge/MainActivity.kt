1f798e01c81e0f976b9bae1c75f77e02de0177e3
Karim <kggm0116@gmail.com>
2023-05-11 13:24:29 +0300

    Added Russian localization, names are now in string resources.

--- part_1_challenge/src/main/java/ru/kggm/part_1_challenge/MainActivity.kt
+++ part_1_challenge/src/main/java/ru/kggm/part_1_challenge/MainActivity.kt
@@ -3,16 +3,26 @@ package ru.kggm.part_1_challenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView
+import androidx.annotation.StringRes
+import androidx.core.os.LocaleListCompat
 import ru.kggm.part_1_challenge.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var name: String
+
+    @StringRes
+    private var nameId = R.string.name_karim
+
+    private fun getRandomNameId() = listOf(
+        R.string.name_arthur,
+        R.string.name_karim,
+        R.string.name_oleg
+    )
+        .random()
 
     companion object {
-        private const val NAME_BUNDLE_KEY = "name"
+        private const val NAME_ID_BUNDLE_KEY = "nameId"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,23 +30,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        name = savedInstanceState?.getString(NAME_BUNDLE_KEY) ?: getRandomName()
-        setBirthdayText(name)
+        nameId = savedInstanceState?.getInt(NAME_ID_BUNDLE_KEY) ?: getRandomNameId()
+        setBirthdayText()
     }
 
     override fun onStart() {
         super.onStart()
-        setBirthdayText(name)
+        setBirthdayText()
+    }
+
+    override fun onLocalesChanged(locales: LocaleListCompat) {
+        super.onLocalesChanged(locales)
+
+        setBirthdayText()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(NAME_BUNDLE_KEY, name)
+        outState.putInt(NAME_ID_BUNDLE_KEY, nameId)
     }
 
-    private fun setBirthdayText(name: String) {
-        binding.happyBirthdayText.text = getString(R.string.happy_birthday_text_template, name)
+    private fun setBirthdayText() {
+        val localizedName = getString(nameId)
+        binding.happyBirthdayText.text = getString(
+            R.string.happy_birthday_text_template,
+            localizedName
+        )
     }
-
-    private fun getRandomName() = listOf("Karim", "Arthur", "Oleg").random()
 }
