b1f50b6e319095942dddad1660d9e393a64a338c
Karim <kggm0116@gmail.com>
2023-05-11 19:40:05 +0300

    Implemented Challenge 1.2.

--- application/src/main/java/ru/kggm/launcher/LauncherActivity.kt
+++ application/src/main/java/ru/kggm/launcher/LauncherActivity.kt
@@ -7,6 +7,7 @@ import ru.kggm.helloworld.HelloWorldActivity
 import ru.kggm.launcher.databinding.ActivityLauncherBinding
 import ru.kggm.happybirthday.HappyBirthdayActivity
 import ru.kggm.hellotoast.HelloToastActivity
+import ru.kggm.hellotoast_challenge.HelloToastChallengeActivity
 
 class LauncherActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLauncherBinding
@@ -27,6 +28,9 @@ class LauncherActivity : AppCompatActivity() {
         }
         binding.task12HomeworkButton.setOnClickListener {
             startActivity(Intent(this, HelloToastActivity::class.java))
+        }
+        binding.task12ChallengeButton.setOnClickListener {
+            startActivity(Intent(this, HelloToastChallengeActivity::class.java))
         }
     }
 
